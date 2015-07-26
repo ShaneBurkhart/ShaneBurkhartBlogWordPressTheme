1b23353f3582cab249456797b7d5c51370e299aa
Shane Burkhart <shaneburkhart@gmail.com>
2015-07-26 18:14:12 -0500

    Added disqus comments.

--- content.php
+++ content.php
@@ -25,7 +25,17 @@
             echo '<p><a href="' . esc_url(get_permalink()) . '">Read More...</a></p>';
         endif;
     ?>
-    <?php
-    // Add Disqus comments 
-    ?>
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES * * */
+        var disqus_shortname = 'shaneburkhart';
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 </article>		
